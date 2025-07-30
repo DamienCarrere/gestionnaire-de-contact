80fc2e8fd1ac09d72a0eda75cb693565c6505502
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-07-30 23:04:46 +0200

    final push add style for hamburger and fix minor style problems

--- modules/formContact.js
+++ modules/formContact.js
@@ -1,73 +1,71 @@
 export function formulaireContacts(contactsArray) {
-    // -------- Première div contenant le formulaire --------
-    const maPremiereDiv = document.createElement('div');
-    maPremiereDiv.id = "maPremiereDiv";
-    document.body.appendChild(maPremiereDiv);
+	// -------- Première div contenant le formulaire --------
+	const maPremiereDiv = document.createElement("div");
+	maPremiereDiv.id = "maPremiereDiv";
+	document.body.appendChild(maPremiereDiv);
 
-    // ----- Nom -----
-    const labelNom = document.createElement('label');
-    labelNom.textContent = "Nom";
-    labelNom.htmlFor = "inputNom";
-    maPremiereDiv.appendChild(labelNom);
+	// ----- Nom -----
+	const labelNom = document.createElement("label");
+	labelNom.textContent = "Nom";
+	labelNom.htmlFor = "inputNom";
+	maPremiereDiv.appendChild(labelNom);
 
-    const inputNom = document.createElement('input');
-    inputNom.type = 'text';
-    inputNom.id = "inputNom";
-    maPremiereDiv.appendChild(inputNom);
+	const inputNom = document.createElement("input");
+	inputNom.type = "text";
+	inputNom.id = "inputNom";
+	maPremiereDiv.appendChild(inputNom);
 
-    // ----- Prénom -----
-    const labelPrenom = document.createElement('label');
-    labelPrenom.textContent = "Prénom";
-    labelPrenom.htmlFor = "inputPrenom";
-    maPremiereDiv.appendChild(labelPrenom);
+	// ----- Prénom -----
+	const labelPrenom = document.createElement("label");
+	labelPrenom.textContent = "Prénom";
+	labelPrenom.htmlFor = "inputPrenom";
+	maPremiereDiv.appendChild(labelPrenom);
 
-    const inputPrenom = document.createElement('input');
-    inputPrenom.type = 'text';
-    inputPrenom.id = "inputPrenom";
-    maPremiereDiv.appendChild(inputPrenom);
+	const inputPrenom = document.createElement("input");
+	inputPrenom.type = "text";
+	inputPrenom.id = "inputPrenom";
+	maPremiereDiv.appendChild(inputPrenom);
 
-    // ----- Numéro -----
-    const labelNumero = document.createElement('label');
-    labelNumero.textContent = "Numéro";
-    labelNumero.htmlFor = "inputNumero";
-    maPremiereDiv.appendChild(labelNumero);
+	// ----- Numéro -----
+	const labelNumero = document.createElement("label");
+	labelNumero.textContent = "Numéro";
+	labelNumero.htmlFor = "inputNumero";
+	maPremiereDiv.appendChild(labelNumero);
 
-    const inputNumero = document.createElement('input');
-    inputNumero.type = 'number';
-    inputNumero.id = "inputNumero";
-    maPremiereDiv.appendChild(inputNumero);
+	const inputNumero = document.createElement("input");
+	inputNumero.type = "number";
+	inputNumero.id = "inputNumero";
+	maPremiereDiv.appendChild(inputNumero);
 
-    // ----- Bouton Valider -----
-    const boutonValider = document.createElement("button");
-    boutonValider.textContent = "Valider";
-    boutonValider.id = "monBoutonValider";
-    maPremiereDiv.appendChild(boutonValider);
+	// ----- Bouton Valider -----
+	const boutonValider = document.createElement("button");
+	boutonValider.textContent = "Valider";
+	boutonValider.id = "monBoutonValider";
+	maPremiereDiv.appendChild(boutonValider);
 
+	// -------- Événement au clic sur le bouton valider --------
+	boutonValider.addEventListener("click", function () {
+		const nom = inputNom.value;
+		const prenom = inputPrenom.value;
+		const numero = inputNumero.value;
 
-    // -------- Événement au clic sur le bouton valider --------
-    boutonValider.addEventListener("click", function () {
-        const nom = inputNom.value;
-        const prenom = inputPrenom.value;
-        const numero = inputNumero.value;
+		contactsArray.push({ nom, prenom, numero });
+		console.log(contactsArray);
 
-        contactsArray.push({ nom, prenom, numero })
-        console.log(contactsArray);
+		if (!nom || !prenom || !numero) {
+			alert("Veuillez remplir tous les champs.");
+			return;
+		}
 
-        if (!nom || !prenom || !numero) {
-            alert("Veuillez remplir tous les champs.");
-            return;
-        }
+		// const contact = document.createElement('p');
+		// contact.textContent = `Contact : ${nom} ${prenom} - ${numero}`;
+		// listeContacts.appendChild(contact);
 
-        // const contact = document.createElement('p');
-        // contact.textContent = `Contact : ${nom} ${prenom} - ${numero}`;
-        // listeContacts.appendChild(contact);
-
-        // inputNom.value = "";
-        // inputPrenom.value = "";
-        // inputNumero.value = "";
-    });
+		// inputNom.value = "";
+		// inputPrenom.value = "";
+		// inputNumero.value = "";
+	});
 }
-
 
 // -------- Deuxième div Choisir une option --------
 // const maDeuxiemeDiv = document.createElement('div');
@@ -82,4 +80,3 @@ export function formulaireContacts(contactsArray) {
 // const listeContacts = document.createElement('div');
 // listeContacts.id = "listeContacts";
 // maDeuxiemeDiv.appendChild(listeContacts);
-
