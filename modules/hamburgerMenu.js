dd441b025a51a28eb7f081ee482c585b8954be23
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-07-30 19:15:30 +0200

    reset for chloe

--- modules/hamburgerMenu.js
+++ modules/hamburgerMenu.js
@@ -1,34 +1,41 @@
-import { createAndAddElement } from "./createAndAddElement.js";
-
+import { listeContacts } from "./listeContacts.js";
+import { formulaireContacts } from "./formContact.js";
 export function createHamburgerMenu() {
-	let divHamburger = createAndAddElement("div", "");
-	divHamburger.classList.add = "divHamburger";
+	const select = document.createElement("select");
+	document.body.appendChild(select);
 
-	let hamburgerMenu = createAndAddElement("select", "", divHamburger);
-	hamburgerMenu.classList.add = "hamburgerMenu";
+	const options = [
+		{ value: "", text: "Que voulez-vous faire?" },
+		{ value: "list", text: "Lister des contacts" },
+		{ value: "add", text: "Ajouter un contact" },
+		{ value: "count", text: "Voir le nombre de contacts" },
+	];
 
-	let optionDefault = createAndAddElement(
-		"option",
-		"Que voulez-vous faire?",
-		hamburgerMenu
-	);
-	optionDefault.value = "";
-	let optionList = createAndAddElement(
-		"option",
-		"Lister des contacts",
-		hamburgerMenu
-	);
-	optionList.value = "list";
-	let optionAddToList = createAndAddElement(
-		"option",
-		"Ajouter un contact",
-		hamburgerMenu
-	);
-	optionAddToList.value = "addToList";
-	let optionCount = createAndAddElement(
-		"option",
-		"Voir le nombre de contacts",
-		hamburgerMenu
-	);
-	optionCount.value = "count";
+	options.forEach((opt) => {
+		const option = document.createElement("option");
+		option.value = opt.value;
+		option.textContent = opt.text;
+		select.appendChild(option);
+	});
+
+	const pageContainer = document.createElement("div");
+	pageContainer.classList.add("page-container");
+	document.body.appendChild(pageContainer);
+
+	select.addEventListener("change", () => {
+		const value = select.value;
+		const tab = [];
+		pageContainer.innerHTML = "";
+
+		if (value === "list") {
+			pageContainer.innerHTML = listeContacts().outerHTML;
+		} else if (value === "add") {
+			pageContainer.innerHTML = formulaireContacts(tab).outerHTML;
+		} else if (value === "count") {
+			pageContainer.innerHTML =
+				"<h2>Nombre de contacts</h2><p>Vous avez 3 contacts.</p>";
+		}
+
+		select.value = "";
+	});
 }
