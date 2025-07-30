a86767ef9e613dc07f944e2532a619df90af9426
Zorkan Azhret <damien.carrere@protonmail.ch>
2025-07-30 13:37:15 +0200

    add interactive click event for logo hamburger and site logo finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
 import { createAndAddElement } from "./modules/createAndAddElement.js";
-import { createHamburgerMenu } from "./modules/hamburgerMenu.js";
 import { createSiteLogo } from "./modules/siteLogo.js";
 
-createHamburgerMenu();
 createSiteLogo();
